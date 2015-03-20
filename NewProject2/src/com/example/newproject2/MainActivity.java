ff40e41fffdbc82625fb9c6830ceceae3b208e44
amirul <bhuiyan.amirul@yahoo.com>
2015-03-20 12:25:59 +0600

    Add Activity

--- NewProject2/src/com/example/newproject2/MainActivity.java
+++ NewProject2/src/com/example/newproject2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.newproject2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +14,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+public void onClick(View view){
+    	
+    	Intent intent= new Intent(this, SecondActivity.class);
+    	intent.putExtra("URL", "http://www.vogella.com");
+    	startActivity(intent);
+    	
+    }
+    
+    
 }
---
+++ NewProject2/src/com/example/newproject2/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.newproject2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+	}
+
+}
